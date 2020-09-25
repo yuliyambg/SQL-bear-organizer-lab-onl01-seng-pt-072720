3cae32c2e126f58ed1b4d83d07d058dc4cb67e7d
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-25 03:07:30 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "male", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "female", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "male", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "female", "pink", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "male", "green", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 5, "female", "green", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5, "female", "green", "happy", 1);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,17 +1,20 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE Gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT name FROM bears 
+   ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE Alive = 1
+  ORDER BY age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT MAX(age) FROM bears
+   ORDER BY name,age;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
