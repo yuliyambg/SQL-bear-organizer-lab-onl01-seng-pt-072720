2593ef9a38e476c611d18ec83da6f98c1216fcf2
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-25 03:28:56 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(color) FROM bears GROUP BY color
-  ORDER BY color;"
+  ORDER BY color DESC;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
